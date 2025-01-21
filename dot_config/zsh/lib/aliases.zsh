e35d0cc06f46077339d10abdfc3ed1674fce2f15
Lachlan Heywood <lachieh@users.noreply.github.com>
2025-01-21 15:42:23 -0500

    Update .config/zsh/.zplugins Update .config/zsh/lib/aliases.zsh Update .config/zsh/lib/setup.zsh Update .zshenv

--- dot_config/zsh/lib/aliases.zsh
+++ dot_config/zsh/lib/aliases.zsh
@@ -17,6 +17,7 @@ alias chez="chezmoi"
 
 # git aliases
 alias gfom="git fetch origin main"
+alias gfum="git fetch upstream main"
 alias gswm="git switch main"
 alias nah='_with_msg "git reset --hard HEAD" "yeah, nah. ditch all that"'
 alias yolo='_with_msg "git add -A && git commit -m \"$(curl -s https://api.kanye.rest/ | jq '\''.quote'\'')\" "yolo commiting"'
--- dot_config/zsh/lib/setup.zsh
+++ dot_config/zsh/lib/setup.zsh
@@ -3,9 +3,5 @@
 ANTIDOTE="$(brew --prefix)/share/antidote"
 if [ -d "$ANTIDOTE" ]; then
   source "$ANTIDOTE/antidote.zsh"
-  if [ -d "${ZDOTDIR}/.config/zsh" ]; then
-    source "${ZDOTDIR}/.config/zsh/.zplugins.zsh"
-  else
-    source "${HOME}/.zplugins.zsh"
-  fi
+  source "${ZDOTDIR/.config/zsh/.zplugins.zsh:-$HOME/.zplugins.zsh}"
 fi
