c46093ba573b969783de893c926ac0e15816e4e7
Lachlan Heywood <lachieh@users.noreply.github.com>
2025-01-21 16:32:35 -0500

    Update .config/zsh/lib/setup.zsh

--- dot_config/zsh/lib/setup.zsh
+++ dot_config/zsh/lib/setup.zsh
@@ -3,5 +3,8 @@
 ANTIDOTE="$(brew --prefix)/share/antidote"
 if [ -d "$ANTIDOTE" ]; then
   source "$ANTIDOTE/antidote.zsh"
-  source "${ZDOTDIR/.config/zsh/.zplugins.zsh:-$HOME/.zplugins.zsh}"
+  local SRC="${ZDOTDIR:-$HOME}/.zplugins"
+  local DST="${ZDOTDIR:-$HOME}/.zplugins.zsh"
+  if [ ! -f $DST ]; then antidote bundle < $SRC > $DST; fi
+  source $DST
 fi
