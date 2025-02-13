97fb6d62db9eb75e601e16424661d7972b85a281
Lachlan Heywood <lachieh@users.noreply.github.com>
2025-02-13 13:04:44 -0500

    update atuin config

--- dot_config/zsh/plugins/atuin/atuin.plugin.zsh
+++ dot_config/zsh/plugins/atuin/atuin.plugin.zsh
@@ -1,6 +1,7 @@
 #! /bin/zsh
 
 ATUIN_DATA_PATH="${ATUIN_DATA_DIR:-$XDG_DATA_HOME/atuin-zsh}"
+ATUIN_CONFIG_DIR="${ATUIN_CONFIG_DIR:-$XDG_CONFIG_HOME/atuin}"
 
 # Check if atuin is installed
 if [[ $+commands[atuin] ]]; then
@@ -8,7 +9,7 @@ if [[ $+commands[atuin] ]]; then
   mkdir -p "${ATUIN_DATA_PATH}"
   mkdir -p "${ATUIN_DATA_PATH}/compdef"
   # init
-  [[ -f "${ATUIN_DATA_PATH}/init.zsh" ]] || atuin init zsh > "${ATUIN_DATA_PATH}/init.zsh"
+  [[ -f "${ATUIN_DATA_PATH}/init.zsh" ]] || atuin init zsh --disable-up-arrow > "${ATUIN_DATA_PATH}/init.zsh"
   source "${ATUIN_DATA_PATH}/init.zsh"
   # completion
   [[ -f "${ATUIN_DATA_PATH}/compdef/_atuin" ]] || atuin gen-completions --shell zsh > "${ATUIN_DATA_PATH}/compdef/_atuin"
