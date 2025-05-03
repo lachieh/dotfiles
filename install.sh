07e030ca91c860dca0c6b9eb2a0829a64343b358
Lachlan Heywood <lachieh@users.noreply.github.com>
2025-05-03 16:58:48 -0400

    add age and 1password

--- install.sh
+++ install.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
+# shellcheck disable=SC1090
 
 set -eufo pipefail
 
 echo ""
-echo "🤚  This script will setup .dotfiles for you."
+echo "Hi! 🤚 Lachie's dotfiles Installer."
 read -n 1 -r -s -p $'    Press any key to continue or Ctrl+C to abort...\n\n'
 
 # Install Homebrew
-command -v brew >/dev/null 2>&1 || \
-  (echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+if ! command -v brew >/dev/null 2>&1; then
+  echo '🍺  Installing Homebrew'
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 # Install chezmoi
-command -v chezmoi >/dev/null 2>&1 || \
-  (echo '🤖  Installing chezmoi' && brew install chezmoi)
+if ! command -v chezmoi >/dev/null 2>&1; then
+  echo '🤖  Installing chezmoi'
+  brew install chezmoi
+fi
+
+# Install mise
+if ! command -v mise >/dev/null 2>&1; then
+  echo '🧩  Installing mise'
+  brew install mise
+
+  source <(/opt/homebrew/bin/mise activate zsh)
+  source <(/opt/homebrew/bin/mise hook-env -s zsh)
+fi
 
 if [ -d "$HOME/.local/share/chezmoi/.git" ]; then
   echo "🚸  chezmoi already initialized"
