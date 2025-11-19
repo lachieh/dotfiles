ed77584b1e01743c7783e7e678da0909d104d7dd
Lachlan Heywood <lachieh@users.noreply.github.com>
2025-11-19 17:55:52 -0500

    Add .config/1password/ssh/agent.toml Update .config/git/config Update .config/zsh/zmodules/000-1password.zsh Remove .ssh/config Add .ssh/config

--- dot_config/zsh/exact_zmodules/000-1password.zsh
+++ dot_config/zsh/exact_zmodules/000-1password.zsh
@@ -1,12 +1,5 @@
 # 1password completion
 if [ -x "$(command -v op)" ]; then
   eval "$(op completion zsh)"; compdef _op op
-  # 1Password Agent Symlink on macOS
-  if [[ "$(uname)" == "Darwin" ]]; then
-  # check if link has not been created and make sure that agent is enabled
-  if [ ! -L ~/.1password/agent.sock ] && [ -e "${HOME}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" ]; then
-    # create symlink to 1password agent
-    mkdir -p ~/.1password && ln -s ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
-  fi
-  fi
+  export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 fi
