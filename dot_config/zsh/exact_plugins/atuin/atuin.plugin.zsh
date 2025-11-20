3240ca938226d32eaecb0de60d59297dd3347f87
Lachlan Heywood <lachieh@users.noreply.github.com>
2025-11-20 16:14:21 -0500

    Update .config/zsh/plugins/atuin/atuin.plugin.zsh

--- dot_config/zsh/exact_plugins/atuin/atuin.plugin.zsh
+++ dot_config/zsh/exact_plugins/atuin/atuin.plugin.zsh
@@ -14,10 +14,9 @@ if [[ $+commands[atuin] ]]; then
   # completion
   [[ -f "${ATUIN_DATA_PATH}/compdef/_atuin" ]] || atuin gen-completions --shell zsh > "${ATUIN_DATA_PATH}/compdef/_atuin"
   fpath=("${ATUIN_DATA_PATH}/compdef" $fpath)
-  
   # auth
-  # if not logged in, log in, (`atuin account verify' returns non-zero exit code if not logged in)
-  if ! atuin account verify &>/dev/null; then
+  if [[ $(atuin account verify) -ne 0 ]]; then
+    echo "Logging into atuin account from 1Password..."
     OP_VAULT_UUID="c5pj6izhhuuirobusafxvnkqau"
     OP_ITEM_UUID="omx4nfxuac33q6v6g7fnr2pznq"
     OP_PATH="op://$OP_VAULT_UUID/$OP_ITEM_UUID"
