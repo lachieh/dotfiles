9d94b340f6959eb93f9373c6de102c6b90a08d6a
Lachlan Heywood <lachieh@users.noreply.github.com>
2025-11-21 18:46:58 -0500

    Update .config/zsh/zmodules/000-1password.zsh

--- dot_config/zsh/exact_zmodules/000-1password.zsh
+++ dot_config/zsh/exact_zmodules/000-1password.zsh
@@ -2,4 +2,8 @@
 if [ -x "$(command -v op)" ]; then
   eval "$(op completion zsh)"; compdef _op op
   export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+  # 1password CLI plugins. See: https://developer.1password.com/docs/cli/shell-plugins
+  if [ -f "${XDG_CONFIG_HOME}/op/plugins.sh" ]; then
+    source "${XDG_CONFIG_HOME}/op/plugins.sh"
+  fi
 fi
