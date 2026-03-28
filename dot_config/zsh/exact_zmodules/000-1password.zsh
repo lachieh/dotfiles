4df48ddba5168091c7a1b04ad950a49a573c563d
Lachlan Heywood <lachieh@users.noreply.github.com>
2026-03-28 14:07:19 -0400

    Update .config/zsh/zmodules/000-1password.zsh Update .ssh/config

--- dot_config/zsh/exact_zmodules/000-1password.zsh
+++ dot_config/zsh/exact_zmodules/000-1password.zsh
@@ -1,7 +1,12 @@
+# shellcheck disable=SC2148
+
 # 1password completion
-if [ -x "$(command -v op)" ]; then
+if [ -z "$PS1" ] || [ -z "$SSH_CLIENT" ]; then
+  # if we're in a vscode remote session, or we're non-interactive, or in ssh, don't set up 1password CLI
+  return
+elif [ -x "$(command -v op)" ]; then
   eval "$(op completion zsh)"; compdef _op op
-  if [ -z $SSH_TTY ]; then
+  if [ -z "${SSH_TTY}" ]; then
     export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
   fi
   # 1password CLI plugins. See: https://developer.1password.com/docs/cli/shell-plugins
